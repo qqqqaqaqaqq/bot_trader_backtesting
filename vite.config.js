0aae4747d84b4e2f2a6146c01bd9eb1da00b5a07
HONG <jooimtn@naver.com>
2025-12-19 13:26:01 +0900

    vite, pacakage

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bot_trader_backtesting/',
   server: {
     port: 24830
   }
