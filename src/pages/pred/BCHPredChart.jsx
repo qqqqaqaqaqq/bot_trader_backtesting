7aa31d51aae35a5f25f8b9e68e2c0e586d288309
HONG <jooimtn@naver.com>
2026-01-06 17:10:21 +0900

    오류수정

--- src/pages/pred/BCHPredChart.jsx
+++ src/pages/pred/BCHPredChart.jsx
@@ -30,17 +30,15 @@ export default function Predicted({ DATA, series }) {
 
   useEffect(() => {
     if ((series && series.length > 0) && (DATA && DATA.length > 0)) {
-      // real: 그냥 날짜만 통일
       const realDATA = DATA.map(item => ({
         x: item.x.split("T")[0],
         y: item.y
       }));
 
-      // pred: type별로 날짜만 통일
       const predSeries = series.map(predType => ({
         name: `Predicted % Change (${predType.name})`,
         data: predType.data.map(item => ({
-          x: item.x.split("T")[0], // YYYY-MM-DD로 통일
+          x: item.x.split("T")[0],
           y: item.y
         }))
       }));
@@ -50,7 +48,8 @@ export default function Predicted({ DATA, series }) {
         { name: "Real % Change", data: realDATA }
       ];
 
-      setCombine(combinedSeries);
+      // 비동기적으로 setState 호출
+      Promise.resolve().then(() => setCombine(combinedSeries));
     }
   }, [series, DATA]);
 
--- src/pages/pred/XRPPredChart.jsx
+++ src/pages/pred/XRPPredChart.jsx
@@ -28,31 +28,30 @@ export default function Predicted({ DATA, series }) {
     grid: { strokeDashArray: 4 },
   };
 
-  useEffect(() => {
-    if ((series && series.length > 0) && (DATA && DATA.length > 0)) {
-      // real: 그냥 날짜만 통일
-      const realDATA = DATA.map(item => ({
+useEffect(() => {
+  if ((series && series.length > 0) && (DATA && DATA.length > 0)) {
+    const realDATA = DATA.map(item => ({
+      x: item.x.split("T")[0],
+      y: item.y
+    }));
+
+    const predSeries = series.map(predType => ({
+      name: `Predicted % Change (${predType.name})`,
+      data: predType.data.map(item => ({
         x: item.x.split("T")[0],
         y: item.y
-      }));
+      }))
+    }));
 
-      // pred: type별로 날짜만 통일
-      const predSeries = series.map(predType => ({
-        name: `Predicted % Change (${predType.name})`,
-        data: predType.data.map(item => ({
-          x: item.x.split("T")[0], // YYYY-MM-DD로 통일
-          y: item.y
-        }))
-      }));
+    const combinedSeries = [
+      ...predSeries,
+      { name: "Real % Change", data: realDATA }
+    ];
 
-      const combinedSeries = [
-        ...predSeries,
-        { name: "Real % Change", data: realDATA }
-      ];
-
-      setCombine(combinedSeries);
-    }
-  }, [series, DATA]);
+    // 비동기적으로 setState 호출
+    Promise.resolve().then(() => setCombine(combinedSeries));
+  }
+}, [series, DATA]);
 
   return (
     <div className="frame">
